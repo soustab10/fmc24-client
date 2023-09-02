00c149be680d131034b81fa360c96eb985153eaa
Soustab Haldar <soustabhaldar10@gmail.com>
2023-09-03 03:16:23 +0530

    fix err

--- src/pages/landingpage/Header.jsx
+++ src/pages/landingpage/Header.jsx
@@ -13,9 +13,9 @@ const Header = () => {
         <a href="" className='links-header w-24 place-self-center'>team</a>
         <a href="" className='links-header w-16 place-self-center'>
           <svg xmlns="http://www.w3.org/2000/svg" width="27.922" height="11.688" viewBox="0 0 30 14" fill="none">
-          <path d="M1 1.15576H28.9218" stroke="white" stroke-width="1.94803" stroke-linecap="round"/>
-          <path d="M1 7H28.9218" stroke="white" stroke-width="1.94803" stroke-linecap="round"/>
-          <path d="M1 12.8442H28.9218" stroke="white" stroke-width="1.94803" stroke-linecap="round"/>
+          <path d="M1 1.15576H28.9218" stroke="white" strokeWidth="1.94803" stroke-linecap="round"/>
+          <path d="M1 7H28.9218" stroke="white" strokeWidth="1.94803" stroke-linecap="round"/>
+          <path d="M1 12.8442H28.9218" stroke="white" strokeWidth="1.94803" stroke-linecap="round"/>
           </svg>
         </a>
       </div>
--- src/pages/landingpage/Sponsors.jsx
+++ src/pages/landingpage/Sponsors.jsx
@@ -1,62 +1,88 @@
-import React from 'react'
-import { Container } from 'react-bootstrap';
-import Carousel from 'react-multi-carousel';
-import Classes from '@/styles/Sponsor.module.css';
-import 'react-multi-carousel/lib/styles.css';
+import React from "react";
+import { Container } from "react-bootstrap";
+import Carousel from "react-multi-carousel";
+import Classes from "@/styles/Sponsor.module.css";
+import Image from "next/image";
+import "react-multi-carousel/lib/styles.css";
 const Sponsors = () => {
-  
   const responsive = {
     superLargeDesktop: {
       // the naming can be any, depends on you.
       breakpoint: { max: 4000, min: 3000 },
-      items: 5
+      items: 5,
     },
     desktop: {
       breakpoint: { max: 3000, min: 1024 },
-      items: 4
+      items: 4,
     },
     tablet: {
       breakpoint: { max: 1024, min: 681 },
-      items: 2
+      items: 2,
     },
     mobile: {
       breakpoint: { max: 680, min: 0 },
-      items: 1
-    }
+      items: 1,
+    },
   };
   return (
-   <>
-  <section className={Classes.sponsor} id ='sponsors' >
-    
-    <Container>
-    <div className={Classes.sponsor_bx}>
-      <h2 className={Classes.heading}>SPONSORS</h2>
-    <Carousel responsive={responsive} infinite={true} autoPlay={true}
-  autoPlaySpeed={2000} className="slider">
-      <div className={Classes.item}>
-      <img  alt="Rectangle" src="https://images.unsplash.com/photo-1535713875002-d1d0cf377fde?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxjb2xsZWN0aW9uLXBhZ2V8MXw3NjA4Mjc3NHx8ZW58MHx8fHx8&w=1000&q=80"/>
-    </div>
-    <div className={Classes.item}>
-      <img alt="Rectangle" src="https://images.unsplash.com/photo-1525351326368-efbb5cb6814d?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=2680&q=80" />
-    </div>
-    <div className={Classes.item} >
-      <img  alt="Rectangle" src="https://images.unsplash.com/photo-1525351326368-efbb5cb6814d?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=2680&q=80" />
-    </div>
-    <div className={Classes.item}>
-      <img  alt="Rectangle" src="https://images.unsplash.com/photo-1525351326368-efbb5cb6814d?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=2680&q=80" />
-    </div>
-    <div className={Classes.item}>
-      <img  alt="Rectangle" src="https://images.unsplash.com/photo-1525351326368-efbb5cb6814d?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=2680&q=80" />
-    </div>  
-    </Carousel>    
-   
-    </div>    
-    </Container> 
-    
+    <>
+      <section className={Classes.sponsor} id="sponsors">
+        <Container>
+          <div className={Classes.sponsor_bx}>
+            <h2 className={Classes.heading}>SPONSORS</h2>
+            <Carousel
+              responsive={responsive}
+              infinite={true}
+              autoPlay={true}
+              autoPlaySpeed={2000}
+              className="slider"
+            >
+              <div className={Classes.item}>
+                <Image
+                  src="https://images.unsplash.com/photo-1535713875002-d1d0cf377fde?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxjb2xsZWN0aW9uLXBhZ2V8MXw3NjA4Mjc3NHx8ZW58MHx8fHx8&w=1000&q=80"
+                  alt="Rectangle"
+                  width={100}
+                  height={100}
+                />
+              </div>
+              <div className={Classes.item}>
+                <Image
+                  src="https://images.unsplash.com/photo-1535713875002-d1d0cf377fde?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxjb2xsZWN0aW9uLXBhZ2V8MXw3NjA4Mjc3NHx8ZW58MHx8fHx8&w=1000&q=80"
+                  alt="Rectangle"
+                  width={100}
+                  height={100}
+                />
+              </div>
+              <div className={Classes.item}>
+                <Image
+                  src="https://images.unsplash.com/photo-1535713875002-d1d0cf377fde?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxjb2xsZWN0aW9uLXBhZ2V8MXw3NjA4Mjc3NHx8ZW58MHx8fHx8&w=1000&q=80"
+                  alt="Rectangle"
+                  width={100}
+                  height={100}
+                />
+              </div>
+              <div className={Classes.item}>
+                <Image
+                  src="https://images.unsplash.com/photo-1535713875002-d1d0cf377fde?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxjb2xsZWN0aW9uLXBhZ2V8MXw3NjA4Mjc3NHx8ZW58MHx8fHx8&w=1000&q=80"
+                  alt="Rectangle"
+                  width={100}
+                  height={100}
+                />
+              </div>
+              <div className={Classes.item}>
+                <Image
+                  src="https://images.unsplash.com/photo-1535713875002-d1d0cf377fde?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxjb2xsZWN0aW9uLXBhZ2V8MXw3NjA4Mjc3NHx8ZW58MHx8fHx8&w=1000&q=80"
+                  alt="Rectangle"
+                  width={100}
+                  height={100}
+                />
+              </div>
+            </Carousel>
+          </div>
+        </Container>
+      </section>
+    </>
+  );
+};
 
-  </section>
-   </>
-  )
-}
-
-export default Sponsors
+export default Sponsors;
