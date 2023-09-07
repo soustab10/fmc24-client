c5d1fcbaf035a936b6192834d91e6dd8f1985be1
Soustab Haldar <soustabhaldar10@gmail.com>
2023-09-07 05:54:31 +0530

    marquee added

--- src/pages/landingpage/OAT.jsx
+++ src/pages/landingpage/OAT.jsx
@@ -7,27 +7,30 @@ const OAT = () => {
       <div className={Classes.container_row}>
         <div className={Classes.layer1}>
           <h2 className={Classes.css1}>OPEN AIR</h2>
-        </div>
-        <div className={Classes.layer2}>
-          <h2 className={Classes.css3}>OPEN AIR</h2>
-        </div>
-      </div>
-      <div className={Classes.container_row}>
-        <div className={Classes.layer1}>
           <h2 className={Classes.css2}>THEATRE</h2>
-        </div>
-        <div className={Classes.layer2}>
-          <h2 className={Classes.css4}>THEATRE</h2>
-        </div>
-      </div>
-
-      <Image
+          <Image
         src="/images/oatbkg.png"
         width={1000}
         height={5000}
         className={Classes.cssi}
         alt="OAT"
       />
+        </div>
+        <div className={Classes.layer2}>
+          <h2 className={Classes.css3}>OPEN AIR</h2>
+          <h2 className={Classes.css4}>THEATRE</h2>
+        </div>
+      </div>
+      {/* <div className={Classes.container_row}>
+        <div className={Classes.layer1}>
+          <h2 className={Classes.css2}>THEATRE</h2>
+        </div>
+        <div className={Classes.layer2}>
+          <h2 className={Classes.css4}>THEATRE</h2>
+        </div>
+      </div> */}
+
+      
     </center>
   );
 };
