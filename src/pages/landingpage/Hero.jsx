ff85c895c378efaeb9c40810e5b157bedf3097c3
shubham <shubham@shubhams-MacBook-Air.local>
2023-09-09 15:54:55 +0530

    video hero section

--- src/pages/landingpage/Hero.jsx
+++ src/pages/landingpage/Hero.jsx
@@ -1,11 +1,28 @@
 import React from 'react'
 import Classes from './styles/Hero.module.css'
 import Design from "./hoverDesign/design"
+import Header from './Header';
+const VideoWithText = () => {
+  return (
+    <div className={Classes.videoContainer}>
+      <video autoPlay loop muted className={Classes.video}>
+        <source src="/Final.mp4" type="video/mp4" />
+      </video>
+      <div 
+      className={Classes.textOverlay}
+      >
+        <h1 
+        >Euphoria Of Expression</h1>
+      </div>
+    </div>
+  );
+};
 const Hero = () => {
   return (
     <>
     <div className={Classes.hero_text}>
-    Hero Section
+    {/* <Header/> */}
+      <VideoWithText/>
     </div>
      
     </>
@@ -13,3 +30,5 @@ const Hero = () => {
 }
 
 export default Hero
+
+
