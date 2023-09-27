7763eed2ce6f4f7fb78a7f3e501de8b80265f507
Shubham Kumar <shubham.kumar.min21@itbhu.ac.in>
2023-09-27 22:58:21 +0530

    deployment err

--- src/pages/landingpage/Header.jsx
+++ src/pages/landingpage/Header.jsx
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from "react";
 import Link from "next/link";
-import Image from "next/image";
+import Image from "next/legacy/image";
 // import Classes from "./styles/Header.module.css";
 
 const navbarGlass = {
