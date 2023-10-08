57aa5b60a2fee91b07d627a5c046352d5b28c86d
Soustab Haldar <soustabhaldar10@gmail.com>
2023-10-08 09:48:39 +0530

    json str

--- src/pages/EventPages/Outreach/index.jsx
+++ src/pages/EventPages/Outreach/index.jsx
@@ -49,7 +49,7 @@ const Photography = () => {
     <p class="text-justify my-3.5 h-[180px]">
     The participants need to make an advertisement on the provided, within 36 hrs. The themes for the same would be released at the start of theevent and participants need to submit their entries in due time.
     </p>
-    <a href="https://drive.google.com/file/d/1gnNykqXp5YDkPydygZo4D-IhukmH9KH4/view?usp=drive_link" className="text-white-800 my-3.5 italic hover:underline">VIEW PROBLEM STATEMENT</a>
+    <a href="https://drive.google.com/file/d/1gnNykqXp5YDkPydygZo4D-IhukmH9KH4/view?usp=drive_link" className="text-white-800 my-3.5 italic hover:underline">View Problem Statement</a>
     <div className="my-3.5 align-middle justify-evenly">
         <button className="border p-2 border-white rounded-[15px] hover:text-[15px] hover:font-semibold transition-all duration-5000 ease-in-out ">REGISTRATION WILL BE LIVE SOON</button>
     </div>
@@ -63,7 +63,7 @@ const Photography = () => {
     You have to make a video log in the range of 60-600 seconds. It can be about anything- visiting nearby attractions, indulging in festive activities,pulling pranks on people, etc. - as long as it is exciting and in which you can show the theme “COLOUR OF MY CITY”.
     </p>
     <a href="https://drive.google.com/file/d/1sB71TnZAhPaZH3NUMgVYNG2HqgQYBkQ-/view?usp=drive_link
-" className="text-white-800 my-3.5 italic hover:underline">VIEW PROBLEM STATEMENT</a>
+" className="text-white-800 my-3.5 italic hover:underline">View Problem Statement</a>
     <div className="my-3.5 align-middle justify-evenly">
         <button className="border p-2 border-white rounded-[15px] hover:text-[15px] hover:font-semibold transition-all duration-5000 ease-in-out ">REGISTRATION WILL BE LIVE SOON</button>
     </div>
@@ -76,7 +76,7 @@ const Photography = () => {
     <p class="text-justify my-3.5 h-[180px]">
     You will have to give a live solo open-mic performance of your rant. It could be a monologue, an interview, a prank or anything else that can captivate the interest of the public
     </p>
-    <a href="https://drive.google.com/file/d/1xUAcYefHYvTkBAYSGQ9ROoLctT9ddDwO/view?usp=drive_link" className="text-white-800 my-3.5 italic hover:underline">VIEW PROBLEM STATEMENT</a>
+    <a href="https://drive.google.com/file/d/1xUAcYefHYvTkBAYSGQ9ROoLctT9ddDwO/view?usp=drive_link" className="text-white-800 my-3.5 italic hover:underline">View Problem Statement</a>
     <div className="my-3.5 align-middle justify-evenly">
         <button className="border p-2 border-white rounded-[12.5px] hover:text-[15px] hover:font-semibold transition-all duration-5000 ease-in-out ">REGISTRATION WILL BE LIVE SOON</button>
     </div>
@@ -89,7 +89,7 @@ const Photography = () => {
     <p class="text-justify my-3.5 h-[180px]">
     Step into the world of storytelling through lenses and frames, where truth meets creativity, and reality is transformed into compelling narratives. Join us for &apos;Capturing Reality: The Art of Documentary Filmmaking,&apos; a captivating and enlightening documentary-making event that dives deep into the fascinating realm of non-fiction storytelling. Participants are required to prepare a documentary film on any topic about any genre.
     </p>
-    <a href="https://drive.google.com/file/d/1sZVovLahZ1jiyv31_uyQYAdKmIwyHtZ3/view?usp=drive_link" className="text-white-800 my-3.5 italic hover:underline">VIEW PROBLEM STATEMENT</a>
+    <a href="https://drive.google.com/file/d/1sZVovLahZ1jiyv31_uyQYAdKmIwyHtZ3/view?usp=drive_link" className="text-white-800 my-3.5 italic hover:underline">View Problem Statement</a>
     <div className="my-3.5 align-middle justify-evenly">
         <button className="border p-2 border-white rounded-[15px] hover:text-[15px] hover:font-semibold transition-all duration-5000 ease-in-out ">REGISTRATION WILL BE LIVE SOON</button>
     </div>
