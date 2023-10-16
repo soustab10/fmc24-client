83281c302d3d52019e00bc5107eec01709392573
menacingsoul <adarsh14103@gmail.com>
2023-10-16 21:06:29 +0530

    some design changes

--- src/pages/dashboard/contestCard/ContestCard.jsx
+++ src/pages/dashboard/contestCard/ContestCard.jsx
@@ -1,7 +1,7 @@
 import Image from "next/image";
 const ContestCard = ({ title, imageSrc }) => {
     return (
-      <div className="w-[392px] h-[267px] justify-center my-6 border mx-2 border-white bg-gradient-to-t from-white/10 to-white/30 text-white rounded-[14px] bg-opacity-10 backdrop-blur-lg shadow-xl drop-shadow-xl bg-blend-normal z-10">
+      <div className="w-[392px] h-[267px] justify-center my-6 shadow-amber-100/20 border mx-2 border-white bg-gradient-to-t from-white/10 to-white/30 text-white rounded-[14px] bg-opacity-10 backdrop-blur-lg shadow-xl drop-shadow-xl bg-blend-normal z-10">
         <div className="flex flex-col text-center justify-center px-6 py-4">
           <div className="flex flex-rows justify-around font-bold text-2xl text-black mb-2 my-3.5">
             <div className="mr-4">Contest</div>
--- src/pages/dashboard/workshopCard/WorkshopCard.jsx
+++ src/pages/dashboard/workshopCard/WorkshopCard.jsx
@@ -1,6 +1,6 @@
 const WorkshopCard = ({ title, imageSrc }) => {
     return (
-        <div className="w-[392px] h-[267px] justify-center my-6 border mx-2 border-white bg-gradient-to-t from-white/10 to-white/30 text-white rounded-[14px] bg-opacity-10 backdrop-blur-lg shadow-xl drop-shadow-xl bg-blend-normal z-10">
+        <div className="w-[392px] h-[267px] justify-center my-6 border  shadow-amber-100/20 mx-2 border-white bg-gradient-to-t from-white/10 to-white/30 text-white rounded-[14px] bg-opacity-10 backdrop-blur-lg shadow-xl drop-shadow-xl bg-blend-normal z-10">
         <div className="flex flex-col text-center justify-center px-6 py-4">
           <div className="flex flex-rows justify-around font-bold text-2xl text-black mb-2 my-3.5">
             <div className="mr-4">Workshop</div>
