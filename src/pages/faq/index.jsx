40f38091ef098f3f9a8e788ba241a918af0f8643
Soustab Haldar <soustabhaldar10@gmail.com>
2023-10-24 21:38:03 +0530

    team upd

--- src/pages/faq/index.jsx
+++ src/pages/faq/index.jsx
@@ -1,7 +1,7 @@
 import Header from "../landingpage/Header";
 import Head from "next/head";
 import Footer from "../landingpage/Footer";
-import React ,{useState} from "react";
+import React, { useState } from "react";
 import LayoutR from "../../../components/LayoutRegister";
 import LayoutC from "../../../components/LayoutCampus";
 import LayoutComp from "../../../components/LayoutCompetitions";
@@ -11,69 +11,107 @@ import dataR from "./DataRegistration.json";
 import dataC from "./DataCampus.json";
 import dataA from "./DataAccomodation.json";
 
-
 const divStyle = {
-    minHeight: "50px",
-  };
+  minHeight: "50px",
+};
 
 export default function Faq() {
-  const [active, setActive] = useState([false, false, false, false, false, false, false, false, false,false,false]);
+  const [active, setActive] = useState([
+    false,
+    false,
+    false,
+    false,
+    false,
+    false,
+    false,
+    false,
+    false,
+    false,
+    false,
+  ]);
   const isSomeActive = active.some((element) => element);
   const handleClick = () => {
     isSomeActive
-      ? setActive([false, false, false, false,false, false, false, false, false,false,false])
-      : setActive([true, true, true, true, true, true,true, true, true, true, true,true,true]);
+      ? setActive([
+          false,
+          false,
+          false,
+          false,
+          false,
+          false,
+          false,
+          false,
+          false,
+          false,
+          false,
+        ])
+      : setActive([
+          true,
+          true,
+          true,
+          true,
+          true,
+          true,
+          true,
+          true,
+          true,
+          true,
+          true,
+          true,
+          true,
+        ]);
   };
 
   return (
     <>
-
-        <Header/>
-        <div style={divStyle}></div>
-        <div style={divStyle}></div>
-        <div className="flex flex-row items-center w-full align-middle text-white text-6xl justify-center">FAQs</div>
-        <div style={divStyle}></div>
-      <div className='grid place-items-center w-full'>
-      <LayoutR
-        handleClick={handleClick}
-        isSomeActive={isSomeActive}
-        data={dataR}
-        turn={active}
-        setTurn={setActive}
-      />
-    </div>
-    <div style={divStyle}></div>
-      <div className='grid place-items-center w-full'>
-      <LayoutC
-        handleClick={handleClick}
-        isSomeActive={isSomeActive}
-        data={dataC}
-        turn={active}
-        setTurn={setActive}
-      />
-    </div>
-    <div style={divStyle}></div>
-    <div className='grid place-items-center w-full'>
-      <LayoutComp
-        handleClick={handleClick}
-        isSomeActive={isSomeActive}
-        data={datacomp}
-        turn={active}
-        setTurn={setActive}
-      />
-    </div>
-    <div style={divStyle}></div>
-    <div className='grid place-items-center w-full'>
-      <LayoutA
-        handleClick={handleClick}
-        isSomeActive={isSomeActive}
-        data={dataA}
-        turn={active}
-        setTurn={setActive}
-      />
-    </div>
-    <div style={divStyle}></div>
-    <Footer/>
+      <Header />
+      <div style={divStyle}></div>
+      <div style={divStyle}></div>
+      <div className="flex flex-row items-center w-full align-middle text-white text-6xl justify-center">
+        FAQs
+      </div>
+      <div style={divStyle}></div>
+      <div className="grid place-items-center w-full text-xl">
+        <LayoutR
+          handleClick={handleClick}
+          isSomeActive={isSomeActive}
+          data={dataR}
+          turn={active}
+          setTurn={setActive}
+        />
+      </div>
+      <div style={divStyle}></div>
+      <div className="grid place-items-center w-full text-xl">
+        <LayoutC
+          handleClick={handleClick}
+          isSomeActive={isSomeActive}
+          data={dataC}
+          turn={active}
+          setTurn={setActive}
+        />
+      </div>
+      <div style={divStyle}></div>
+      <div className="grid place-items-center w-full text-xl">
+        <LayoutComp
+          handleClick={handleClick}
+          isSomeActive={isSomeActive}
+          data={datacomp}
+          turn={active}
+          setTurn={setActive}
+        />
+      </div>
+      <div style={divStyle}></div>
+      <div className="grid place-items-center w-full text-xl">
+        <LayoutA
+          handleClick={handleClick}
+          isSomeActive={isSomeActive}
+          data={dataA}
+          turn={active}
+          setTurn={setActive}
+        />
+      </div>
+      <div style={divStyle}></div>
+      <Footer />
     </>
-  )
+  );
 }
