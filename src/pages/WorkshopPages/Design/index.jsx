5bd183a17bc0400194ac636d828cb40bf453289b
Soustab Haldar <soustabhaldar10@gmail.com>
2023-10-24 22:01:08 +0530

    workshop upd

--- src/pages/WorkshopPages/Design/index.jsx
+++ src/pages/WorkshopPages/Design/index.jsx
@@ -4,99 +4,140 @@ import Footer from "../../landingpage/Footer";
 import Link from "next/link";
 import Image from "next/image";
 import Classes from "../indexe.module.css";
-import {useAuth} from "../../../context/auth";
+import { useAuth } from "../../../context/auth";
 import { useEffect } from "react";
 
 const divStyle = {
-    minHeight: "100px",
-  };
-  
-
-const Index = () => {
-  const {state} = useAuth();
-  const {isAuthenticated} = state;
-  const [isLoggedin, setIsLoggedin] = React.useState(false);
-  useEffect(()=>{
-    if(isAuthenticated){
-      setIsLoggedin(true);
-    }
-  },[state])
-    return (
-<div className={Classes.FullPage}>
-            <Header/>
-            <div>
-    <div className="mt-[80px]"><h1 hidden>Hello</h1></div>
-    <div className={Classes.showMobile}>
-    <div className="w-[300px]  h-[125px] flex justify-center items-center"> 
-        <Link href="../workshops"><button className={Classes.backButton}>Back</button></Link>
-         </div>
-     </div>
-      <div className={Classes.MainArea}>
-        
-        <div className={Classes.TopBar}>
-           <div className={Classes.BarIn}>
-             <div className="w-[275px]  h-[125px] flex justify-center items-center glow-effect-blue"> 
-             <Link href="../WorkshopPages/Photography" className="text-[20px] hover:text-[36px] hover:font-semibold transition-all duration-5000 ease-in-out">Photography</Link>
-             </div>
-             {/* <div className="w-[275px]  h-[125px] flex justify-center items-center glow-effect-green">
-             <Link href="../WorkshopPages/Design" className="text-[20px] hover:text-[36px] hover:font-semibold transition-all duration-5000 ease-in-out">Design</Link>
-             </div> */}
-             <div className="w-[275px]  h-[125px] flex justify-center items-center glow-effect-red">
-             <Link href="../WorkshopPages/Animation" className="text-[20px] hover:text-[36px] hover:font-semibold transition-all duration-5000 ease-in-out">Animation</Link>
-             </div>
-             <div className="w-[275px]  h-[125px] flex justify-center items-center glow-effect-yellow">
-             <Link href="../WorkshopPages/Cinematography" className="text-[20px] hover:text-[36px] hover:font-semibold transition-all duration-5000 ease-in-out">Cinematography</Link>
-             </div>
-             {/* <div className="w-[275px]  h-[125px] flex justify-center items-center glow-effect-orange">
-             <Link href="../WorkshopPages/Media" className="text-[20px] hover:text-[36px] hover:font-semibold transition-all duration-5000 ease-in-out">Media</Link>
-             </div> */}
-             {/* <div className="w-[275px]  h-[125px] flex justify-center items-center glow-effect-purple">
-             <Link href="../WorkshopPages/Outreach" className="text-[20px] hover:text-[36px] hover:font-semibold transition-all duration-5000 ease-in-out">Outreach</Link> 
-             </div> */}
-           </div>
-        </div> 
-  <div className={Classes.MajorBod} >
-    <div className={Classes.CardDe}>
-      <div class="flex flex-col text-center justify-center items-center">
-        <Image src={"/Vimal.png"} width={387} height={350} className="rounded-tr-[25px] rounded-tl-[25px]" alt="null"></Image>
-        <div class=" flex flex-rows h-[30px] justify-center font-bold text-3xl"><div className="ml-4 mt-5 font-bold text-[30px] p-[10px]">Product Design</div></div>
-        <a href="https://instagram.com/artloverjv?igshid=NjIwNzIyMDk2Mg==" className="text-black mt-10 italic hover:underline text-[20px]  px-10 p-1">-by Vimal Krishnattri </a>
-        <p class="text-justify mt-10 mx-2  w-[290px]">Vimal Krishnattri is a skilled graphic designer and illustrator. He showcases AI-generated artwork and adept photoshopping on his Instagram account artloverjv.
-        </p>
-        
-        <div className="my-8 align-middle justify-evenly">
-        {isLoggedin ? (
-              <a>
-                  <Link href="../cart">
-                  <button className="border mb-10 px-6 py-2 border-black rounded-[20px] hover:text-[15px] hover:font-semibold transition-all duration-5000 ease-in-out text-black">
-                    Go to Cart
-                  </button>
-                  </Link>
-                  
-                </a>
-              ) : (
-              <a>
-                <Link href="../login">
-                <button className="border mb-10 px-6 py-2 border-black rounded-[20px] hover:text-[15px] hover:font-semibold transition-all duration-5000 ease-in-out text-black">
-                    Login
-                  </button>
-                </Link>
-                </a>
-              )}
-        </div>
-      </div>
-    </div>
-   
-    <div className={Classes.Ghost}></div>
-    <div className={Classes.Ghost}></div>
-  </div>
-</div>
-    
-            </div>
-            <Footer/>
-        </div>
-        );
+  minHeight: "100px",
 };
 
-export default Index
+const Index = () => {
+  const { state } = useAuth();
+  const { isAuthenticated } = state;
+  const [isLoggedin, setIsLoggedin] = React.useState(false);
+  useEffect(() => {
+    if (isAuthenticated) {
+      setIsLoggedin(true);
+    }
+  }, [state]);
+  return (
+    <div className={Classes.FullPage}>
+      <Header />
+      <div>
+        <div className="mt-[80px]">
+          <h1 hidden>Hello</h1>
+        </div>
+        <div className={Classes.showMobile}>
+          <div className="w-[300px]  h-[125px] flex justify-center items-center">
+            <Link href="../workshops">
+              <button className={Classes.backButton}>Back</button>
+            </Link>
+          </div>
+        </div>
+        <div className={Classes.MainArea}>
+          <div className={Classes.TopBar}>
+            <div className={Classes.BarIn}>
+              <div className="w-[275px]  h-[125px] flex justify-center items-center glow-effect-blue">
+                <Link
+                  href="../WorkshopPages/Photography"
+                  className="text-[20px] hover:text-[36px] hover:font-semibold transition-all duration-5000 ease-in-out"
+                >
+                  Photography
+                </Link>
+              </div>
+              <div className="w-[275px]  h-[125px] flex justify-center items-center glow-effect-green">
+                <Link
+                  href="../WorkshopPages/Design"
+                  className="text-[20px] hover:text-[36px] hover:font-semibold transition-all duration-5000 ease-in-out"
+                >
+                  Design
+                </Link>
+              </div>
+              <div className="w-[275px]  h-[125px] flex justify-center items-center glow-effect-red">
+                <Link
+                  href="../WorkshopPages/Animation"
+                  className="text-[20px] hover:text-[36px] hover:font-semibold transition-all duration-5000 ease-in-out"
+                >
+                  Animation
+                </Link>
+              </div>
+              <div className="w-[275px]  h-[125px] flex justify-center items-center glow-effect-yellow">
+                <Link
+                  href="../WorkshopPages/Cinematography"
+                  className="text-[20px] hover:text-[36px] hover:font-semibold transition-all duration-5000 ease-in-out"
+                >
+                  Cinematography
+                </Link>
+              </div>
+              <div className="w-[275px]  h-[125px] flex justify-center items-center glow-effect-orange">
+                <Link
+                  href="../WorkshopPages/Media"
+                  className="text-[20px] hover:text-[36px] hover:font-semibold transition-all duration-5000 ease-in-out"
+                >
+                  Media
+                </Link>
+              </div>
+              {/* <div className="w-[275px]  h-[125px] flex justify-center items-center glow-effect-purple">
+             <Link href="../WorkshopPages/Outreach" className="text-[20px] hover:text-[36px] hover:font-semibold transition-all duration-5000 ease-in-out">Outreach</Link> 
+             </div> */}
+            </div>
+          </div>
+          <div className={Classes.MajorBod}>
+            <div className={Classes.CardDe}>
+              <div class="flex flex-col text-center justify-center items-center">
+                <Image
+                  src={"/Ankur.jpg"}
+                  width={387}
+                  height={350}
+                  className="rounded-tr-[25px] rounded-tl-[25px]"
+                  alt="null"
+                ></Image>
+                <div class=" flex flex-rows h-[30px] justify-center font-bold text-3xl">
+                  <div className="ml-4 mt-5 font-bold text-[30px] p-[10px]">
+                    Doodling Workshop
+                  </div>
+                </div>
+                <a
+                  href="https://www.instagram.com/doodle_nath/"
+                  className="text-black mt-10 italic hover:underline text-[20px]  px-8 p-1"
+                >
+                  -by Ankur Debnath{" "}
+                </a>
+                <p class="text-justify mt-4 mx-2  w-[290px]">
+                  Ankur Debnath, a doodle artist, specializes in crafting
+                  endearing human doodles that infuse joy into every creation.
+                </p>
 
+                <div className="my-8 align-middle justify-evenly">
+                  {isLoggedin ? (
+                    <a>
+                      <Link href="../cart">
+                        <button className="border mb-10 px-6 py-2 border-black rounded-[20px] hover:text-[15px] hover:font-semibold transition-all duration-5000 ease-in-out text-black">
+                          Go to Cart
+                        </button>
+                      </Link>
+                    </a>
+                  ) : (
+                    <a>
+                      <Link href="../login">
+                        <button className="border mb-10 px-6 py-2 border-black rounded-[20px] hover:text-[15px] hover:font-semibold transition-all duration-5000 ease-in-out text-black">
+                          Login
+                        </button>
+                      </Link>
+                    </a>
+                  )}
+                </div>
+              </div>
+            </div>
+
+            <div className={Classes.Ghost}></div>
+            <div className={Classes.Ghost}></div>
+          </div>
+        </div>
+      </div>
+      <Footer />
+    </div>
+  );
+};
+
+export default Index;
--- src/pages/WorkshopPages/Media/index.jsx
+++ src/pages/WorkshopPages/Media/index.jsx
@@ -4,98 +4,189 @@ import Footer from "../../landingpage/Footer";
 import Link from "next/link";
 import Image from "next/image";
 import Classes from "../indexe.module.css";
-import {useAuth} from "../../../context/auth";
+import { useAuth } from "../../../context/auth";
 import { useEffect } from "react";
 
 const divStyle = {
-    minHeight: "100px",
-  };
-  
-
-const Index = () => {
-  const {state} = useAuth();
-  const {isAuthenticated} = state;
-  const [isLoggedin, setIsLoggedin] = React.useState(false);
-  useEffect(()=>{
-    if(isAuthenticated){
-      setIsLoggedin(true);
-    }
-  },[state])
-    return (
-<div className={Classes.FullPage}>
-            <Header/>
-            <div>
-    <div className="mt-[80px]"><h1 hidden>Hello</h1></div>
-    <div className={Classes.showMobile}>
-    <div className="w-[300px]  h-[125px] flex justify-center items-center"> 
-        <Link href="../workshops"><button className={Classes.backButton}>Back</button></Link>
-         </div>
-     </div>
-      <div className={Classes.MainArea}>
-        
-        <div className={Classes.TopBar}>
-           <div className={Classes.BarIn}>
-             <div className="w-[275px]  h-[125px] flex justify-center items-center glow-effect-blue"> 
-             <Link href="../WorkshopPages/Photography" className="text-[20px] hover:text-[36px] hover:font-semibold transition-all duration-5000 ease-in-out">Photography</Link>
-             </div>
-             {/* <div className="w-[275px]  h-[125px] flex justify-center items-center glow-effect-green">
-             <Link href="../WorkshopPages/Design" className="text-[20px] hover:text-[36px] hover:font-semibold transition-all duration-5000 ease-in-out">Design</Link>
-             </div> */}
-             <div className="w-[275px]  h-[125px] flex justify-center items-center glow-effect-red">
-             <Link href="../WorkshopPages/Animation" className="text-[20px] hover:text-[36px] hover:font-semibold transition-all duration-5000 ease-in-out">Animation</Link>
-             </div>
-             <div className="w-[275px]  h-[125px] flex justify-center items-center glow-effect-yellow">
-             <Link href="../WorkshopPages/Cinematography" className="text-[20px] hover:text-[36px] hover:font-semibold transition-all duration-5000 ease-in-out">Cinematography</Link>
-             </div>
-             {/* <div className="w-[275px]  h-[125px] flex justify-center items-center glow-effect-orange">
-             <Link href="../WorkshopPages/Media" className="text-[20px] hover:text-[36px] hover:font-semibold transition-all duration-5000 ease-in-out">Media</Link>
-             </div> */}
-             {/* <div className="w-[275px]  h-[125px] flex justify-center items-center glow-effect-purple">
-             <Link href="../WorkshopPages/Outreach" className="text-[20px] hover:text-[36px] hover:font-semibold transition-all duration-5000 ease-in-out">Outreach</Link> 
-             </div> */}
-           </div>
-        </div> 
-  <div className={Classes.MajorBod} >
-    <div className={Classes.CardMe}>
-      <div class="flex flex-col text-center justify-center items-center  ">
-        <Image src={"/Saurabh.png"} width={387} height={350} alt="null"  className="rounded-tr-[25px] rounded-tl-[25px]"></Image>
-        <div className=" flex flex-rows h-[30px] justify-center font-bold text-3xl"><div className="ml-4  font-bold text-[30px] p-[10px]">Panel Discussion</div></div>
-        <a href="https://instagram.com/saurabhshuklajourno?igshid=MzRlODBiNWFlZA==" className="text-black mt-5 italic hover:underline text-[20px]  px-10 p-1 ">-by Saurabh Shukla</a>
-        <p className="text-justify mx-2 mt-3 w-[310px]">Saurabh Shukla is a renowned journalist and TV anchor . He is  the Founder and Editor-in-Chief of NewsMobile , which is an application that helps you to fact check rumors to prevent misinformation from spreading amongst the masses , apart from keeping you updated with the daily news across various domains in different parts of the world. He is also an expert in Geopolitics. 
-        </p>
-        
-        <div className="my-4 align-middle justify-evenly">
-        {isLoggedin ? (
-              <a>
-                  <Link href="../cart">
-                  <button className="border mb-10 px-6 py-2 border-black rounded-[20px] hover:text-[15px] hover:font-semibold transition-all duration-5000 ease-in-out text-black">
-                    Go to Cart
-                  </button>
-                  </Link>
-                  
-                </a>
-              ) : (
-              <a>
-                <Link href="../login">
-                <button className="border mb-10 px-6 py-2 border-black rounded-[20px] hover:text-[15px] hover:font-semibold transition-all duration-5000 ease-in-out text-black">
-                    Login
-                  </button>
-                </Link>
-                </a>
-              )}
-        </div>
-      </div>
-    </div>
-    <div className={Classes.Ghost}></div>
-    <div className={Classes.Ghost}></div>
-  </div>
-</div>
-    
-            </div>
-            <Footer/>
-        </div>
-        );
+  minHeight: "100px",
 };
 
-export default Index
+const Index = () => {
+  const { state } = useAuth();
+  const { isAuthenticated } = state;
+  const [isLoggedin, setIsLoggedin] = React.useState(false);
+  useEffect(() => {
+    if (isAuthenticated) {
+      setIsLoggedin(true);
+    }
+  }, [state]);
+  return (
+    <div className={Classes.FullPage}>
+      <Header />
+      <div>
+        <div className="mt-[80px]">
+          <h1 hidden>Hello</h1>
+        </div>
+        <div className={Classes.showMobile}>
+          <div className="w-[300px]  h-[125px] flex justify-center items-center">
+            <Link href="../workshops">
+              <button className={Classes.backButton}>Back</button>
+            </Link>
+          </div>
+        </div>
+        <div className={Classes.MainArea}>
+          <div className={Classes.TopBar}>
+            <div className={Classes.BarIn}>
+              <div className="w-[275px]  h-[125px] flex justify-center items-center glow-effect-blue">
+                <Link
+                  href="../WorkshopPages/Photography"
+                  className="text-[20px] hover:text-[36px] hover:font-semibold transition-all duration-5000 ease-in-out"
+                >
+                  Photography
+                </Link>
+              </div>
+              <div className="w-[275px]  h-[125px] flex justify-center items-center glow-effect-green">
+                <Link
+                  href="../WorkshopPages/Design"
+                  className="text-[20px] hover:text-[36px] hover:font-semibold transition-all duration-5000 ease-in-out"
+                >
+                  Design
+                </Link>
+              </div>
+              <div className="w-[275px]  h-[125px] flex justify-center items-center glow-effect-red">
+                <Link
+                  href="../WorkshopPages/Animation"
+                  className="text-[20px] hover:text-[36px] hover:font-semibold transition-all duration-5000 ease-in-out"
+                >
+                  Animation
+                </Link>
+              </div>
+              <div className="w-[275px]  h-[125px] flex justify-center items-center glow-effect-yellow">
+                <Link
+                  href="../WorkshopPages/Cinematography"
+                  className="text-[20px] hover:text-[36px] hover:font-semibold transition-all duration-5000 ease-in-out"
+                >
+                  Cinematography
+                </Link>
+              </div>
+              <div className="w-[275px]  h-[125px] flex justify-center items-center glow-effect-orange">
+                <Link
+                  href="../WorkshopPages/Media"
+                  className="text-[20px] hover:text-[36px] hover:font-semibold transition-all duration-5000 ease-in-out"
+                >
+                  Media
+                </Link>
+              </div>
+              {/* <div className="w-[275px]  h-[125px] flex justify-center items-center glow-effect-purple">
+             <Link href="../WorkshopPages/Outreach" className="text-[20px] hover:text-[36px] hover:font-semibold transition-all duration-5000 ease-in-out">Outreach</Link> 
+             </div> */}
+            </div>
+          </div>
+          <div className={Classes.MajorBod}>
+            <div className={Classes.CardMe}>
+              <div class="flex flex-col text-center justify-center items-center  ">
+                <Image
+                  src={"/Saurabh.png"}
+                  width={387}
+                  height={350}
+                  alt="null"
+                  className="rounded-tr-[25px] rounded-tl-[25px]"
+                ></Image>
+                <div className=" flex flex-rows h-[30px] justify-center font-bold text-3xl">
+                  <div className="ml-4  font-bold text-[30px] p-[10px]">
+                    Panel Discussion
+                  </div>
+                </div>
+                <a
+                  href="https://instagram.com/saurabhshuklajourno?igshid=MzRlODBiNWFlZA=="
+                  className="text-black mt-5 italic hover:underline text-[20px]  px-10 p-1 "
+                >
+                  -by Saurabh Shukla
+                </a>
+                <p className="text-justify mx-2 mt-3 w-[310px]">
+                  Saurabh Shukla is a renowned journalist and TV anchor . He is
+                  the Founder and Editor-in-Chief of NewsMobile , which is an
+                  application that helps you to fact check rumors to prevent
+                  misinformation from spreading amongst the masses , apart from
+                  keeping you updated with the daily news across various domains
+                  in different parts of the world. He is also an expert in
+                  Geopolitics.
+                </p>
 
+                <div className="my-4 align-middle justify-evenly">
+                  {isLoggedin ? (
+                    <a>
+                      <Link href="../cart">
+                        <button className="border mb-10 px-6 py-2 border-black rounded-[20px] hover:text-[15px] hover:font-semibold transition-all duration-5000 ease-in-out text-black">
+                          Go to Cart
+                        </button>
+                      </Link>
+                    </a>
+                  ) : (
+                    <a>
+                      <Link href="../login">
+                        <button className="border mb-10 px-6 py-2 border-black rounded-[20px] hover:text-[15px] hover:font-semibold transition-all duration-5000 ease-in-out text-black">
+                          Login
+                        </button>
+                      </Link>
+                    </a>
+                  )}
+                </div>
+              </div>
+            </div>
+            <div className={Classes.CardMe}>
+              <div class="flex flex-col text-center justify-center items-center  ">
+                <Image
+                  src={"/sumaira.jpg"}
+                  width={387}
+                  height={350}
+                  alt="null"
+                  className="rounded-tr-[25px] rounded-tl-[25px]"
+                ></Image>
+                <div className=" flex flex-rows h-[30px] justify-center font-bold text-3xl">
+                  <div className="ml-4  font-bold text-[30px] p-[10px]">
+                    Panel Discussion
+                  </div>
+                </div>
+                <a
+                  href="https://twitter.com/sumairakh?t=RJRzFimP3RPpcYWFJ4TVjA&s=09"
+                  className="text-black mt-5 italic hover:underline text-[20px]  px-10 p-1 "
+                >
+                  -by Sumaira Khan 
+                </a>
+                <p className="text-justify mx-2 mt-3 w-[310px]">
+                Sumaira Khan is a renowned TV News anchor, a household name in the Indian TV News Media Industry. She serves as a News Presenter at TV9 Bharatvarsh. Her claim to fame is hosting the debate program “Batana To Padega” on Zee Hindustan. She has worked with well-known news TV stations, including ABP News and Zee Hindustan. Currently, Sumaira hosts TV9 Bharatvarshs popular program, Political Adda.
+                </p>
+
+                <div className="my-4 align-middle justify-evenly">
+                  {isLoggedin ? (
+                    <a>
+                      <Link href="../cart">
+                        <button className="border mb-10 px-6 py-2 border-black rounded-[20px] hover:text-[15px] hover:font-semibold transition-all duration-5000 ease-in-out text-black">
+                          Go to Cart
+                        </button>
+                      </Link>
+                    </a>
+                  ) : (
+                    <a>
+                      <Link href="../login">
+                        <button className="border mb-10 px-6 py-2 border-black rounded-[20px] hover:text-[15px] hover:font-semibold transition-all duration-5000 ease-in-out text-black">
+                          Login
+                        </button>
+                      </Link>
+                    </a>
+                  )}
+                </div>
+              </div>
+            </div>
+            <div className={Classes.Ghost}></div>
+            <div className={Classes.Ghost}></div>
+          </div>
+        </div>
+      </div>
+      <Footer />
+    </div>
+  );
+};
+
+export default Index;
